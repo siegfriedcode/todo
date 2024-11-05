f102d46cde27dce9d0ce9df33c38ad9245180575
Claudio Fuentealba <claudio.fuentealba.bizama@gmail.com>
2024-11-05 10:58:58 -0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask
+
+#
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()  
+#
+
+def create_app():
+    app = Flask(__name__)
+    #app.run(debug=True) 
+
+    app.config.from_mapping(
+        SECRET_KEY='mikey',
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE=os.environ.get('FLASK_DATABASE')
+    )
+
+    from . import db
+
+    db.init_app(app)
+
+    from . import auth
+    from . import todo
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(todo.bp)
+
+
+    @app.route('/hola')
+    def hola():
+        return 'Roma'
+    return app
